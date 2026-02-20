6ce6ca6802d204309699acdc69a8756c3f71b166
Deejay Diabolik <9040397+DjDiabolik@users.noreply.github.com>
2026-02-20 22:38:30 +0100

    Update sw.js
    e anche qua.. 5.4.1 e... luce!

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'boxing-timer-v5.4';
+const CACHE_NAME = 'boxing-timer-v5.4.1';
 const assets = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
